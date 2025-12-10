94df8f0bf7aa276641e1d8f6c4d49151a3460aa7
Pedro Lopes <p.lopes@runtime-revolution.com>
2025-12-10 15:18:00 +0000

    User mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+  def sale_commission
+    @ebook = params[:ebook]
+    seller = @ebook.seller
+    mail(to: seller.user.email, subjet: "One of your ebooks has been purchased")
+  end
+
+  def ebook_statistics
+    @ebook = params[:ebook]
+    seller = @ebook.seller
+    @statistics = @ebook.ebook_statistic
+    mail(to: seller.user.email, subjet: "Ebook Statistics")
+  end
+end
