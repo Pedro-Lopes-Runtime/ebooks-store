d7c8cc39222eb792a6aec74030ac153bddd3d506
Pedro Lopes <p.lopes@runtime-revolution.com>
2025-12-10 15:21:29 +0000

    Preview and purchase ebook actions

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -47,6 +47,25 @@ class EbooksController < ApplicationController
     redirect_back_or_to "/"
   end
 
+  def preview
+    stats = @ebook.ebook_statistic
+    stats.preview_views += 1
+    stats.save
+    redirect_to rails_blob_path(@ebook.preview, dispostion: "preview")
+  end
+
+  def purchase
+    stats = @ebook.ebook_statistic
+    stats.purchases += 1
+    stats.save
+    VisitorStatistic.create(ebook_statistic: @ebook.ebook_statistic, ip: request.remote_ip, browser: Browser.new(request.env["HTTP_USER_AGENT"]).name, location: request.location.country)
+    respond_to do |format|
+      UserMailer.with(ebook: @ebook).sale_commission.deliver_now
+      UserMailer.with(ebook: @ebook).ebook_statistics.deliver_now
+      format.html { redirect_to ebooks_path, notice: "Ebook was successfully purchased." }
+    end
+  end
+
   private
 
   def set_ebook
@@ -54,6 +73,6 @@ class EbooksController < ApplicationController
   end
 
   def ebook_params
-    params.require(:ebook).permit(:title, :description, :author_id, :ebook_status_id, :preview)
+    params.require(:ebook).permit(:title, :description, :author_id, :ebook_status_id, :preview, :seller_id, :price)
   end
 end
