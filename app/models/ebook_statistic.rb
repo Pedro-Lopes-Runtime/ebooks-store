c37dfa3fc5aa553bb4dbe4d6d8f5d719d5725ad8
Pedro Lopes <p.lopes@runtime-revolution.com>
2025-12-11 09:39:28 +0000

    Ebook statistic methods

--- app/models/ebook_statistic.rb
+++ app/models/ebook_statistic.rb
@@ -1,4 +1,18 @@
 class EbookStatistic < ApplicationRecord
   belongs_to :ebook
   has_many :visitor_statistics
+
+  def update_visits
+    self.visits += 1
+    save
+  end
+
+  def update_preview_views
+    self.preview_views += 1
+  end
+
+  def update_purchases
+    raise
+    self.purchases += 1
+  end
 end
